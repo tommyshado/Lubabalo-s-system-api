75da0ea2feaf98b2e7e70b31668cadea273f44fe
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-02 14:35:44 +0200

    created a database table for the stock inventory

--- sql/shoesTable.sql
+++ sql/shoesTable.sql
@@ -7,3 +7,12 @@
         -- shoe_quantity INT
         -- shoe_price INT
         -- shoe_color VARCHAR
+
+create table stock_inventory (
+    shoe_id serial PRIMARY KEY,
+    shoe_name VARCHAR(30) not null,
+    image BYTEA not null,
+    shoe_qty numeric not null,
+    shoe_price numeric not null,
+    shoe_color VARCHAR(30) not null
+)
