d4e284f115b537c546960469e1b469dbcac54889
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-02 10:33:29 +0200

    added routes, and added a routes middleware in the index.js file

--- index.js
+++ index.js
@@ -3,23 +3,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import pgPromise from "pg-promise";
 
-// Services imports
-import shoesService from "./services/shoesService.js";
-
 // Routes imports
 import shoesRoutes from "./routes/shoesRoutes.js";
 
 const app = express();
-const router = express.Router();
-const service = shoesService();
-const route = shoesRoutes(service, router);
-
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// Middleware
+app.use("/api/shoes", shoesRoutes);
 
 // PORT variable
 const PORT = process.env.PORT || 3000;
--- routes/shoesRoutes.js
+++ routes/shoesRoutes.js
@@ -1,8 +1,31 @@
+import shoesService from "../services/shoesService.js";
+import { Router } from "express";
 
+const ShoesService = shoesService();
+const router = Router();
 
+router.get("/", (req, res) => {
+    res.send("shoes route...");
+});
 
-const shoesRoutes = (logic, router) => {
+router.get("/brand/:brandname", (req, res) => {
+    res.send("shoes brand...");
+});
 
-};
+router.get("/brand/size/:size", (req, res) => {
+    res.send("shoes size...");
+});
 
-export default shoesRoutes;
+router.get("/brand/brand/:brandname/size/:size", (req, res) => {
+    res.send("shoes brand and size...");
+});
+
+router.post("/brand/shoes/sold/:id", (req, res) => {
+    console.log("sold shoe...");
+});
+
+router.post("/", (req, res) => {
+    console.log("added a new shoe...");
+});
+
+export default router;
