0e0dc6128c92b0566a58aff1c0ac743a501ecace
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-09 14:59:01 +0200

    fixed line 8

--- validation.js
+++ validation.js
@@ -5,7 +5,7 @@ import joi from "@hapi/joi";
 const schema = {
     name: joi.string().min(6).required(),
     email: joi.string().min(6).required(),
-    password: joi.string(6).required()
+    password: joi.string().min(6).required()
 };
 
 export const registerValidation = (data) => {
