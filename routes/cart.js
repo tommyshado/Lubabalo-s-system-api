bbe21c63f23555401fdfbbf0aab40529c068176b
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-10 23:31:35 +0200

    created routes for getting a shopping cart for a username and adding shoes to the shopping cart

---
+++ routes/cart.js
@@ -0,0 +1,54 @@
+import shoppingCart from "../services/shoppingCart.js"
+import database from "../model/dbConnection.js";
+import { Router } from "express";
+
+// Service instance
+const ShoppingCart = shoppingCart(database);
+// Router instance
+const router = Router();
+
+// Routes
+
+// Router for getting cart for a user
+router.get("/username/:username", async (req, res) => {
+    try {
+        const data = {
+            username: req.params.username
+        };
+        const cart = await ShoppingCart.getCart(data);
+
+        res.json({
+            status: "success",
+            cart: cart
+        })
+        
+    } catch (err) {
+        res.json({
+            status: "error",
+            error: err.stack
+        })
+    };
+});
+
+router.post("/username/:username/shoeId/:shoeId", async (req, res) => {
+    try {
+        const data = {
+            shoeId: req.params.shoeId,
+            username: req.params.username
+        };
+        // Adding to the cart
+        await ShoppingCart.addToCart(data);
+
+        res.json({
+            status: "success"
+        })
+        
+    } catch (err) {
+        res.json({
+            status: "error",
+            error: err.stack
+        })
+    };
+});
+
+export default router;
