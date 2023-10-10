a3176923683aa7c458c0ca5ef8543284e5359a73
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-10-10 23:30:36 +0200

    removed filtering by shoe_id

--- services/shoppingCart.js
+++ services/shoppingCart.js
@@ -1,13 +1,12 @@
 const shoppingCart = (database) => {
-    const getCart = async (details) => {
-        const data = [details.shoeId, details.username];
+    const getCart = async (data) => {
         return await database.manyOrNone(
-            `select * from shopping_cart where shoe_id = '${data[0]}' and username = '${data[1]}'`
+            `select * from shopping_cart where username = '${data.username}'`
         );
     };
 
     const addToCart = async (data) => {
-        const add = [data.username, data.shoeId, data.qty];
+        const add = [data.username, data.shoeId, 1];
         await database.none(
             "insert into shopping_cart (username, shoe_id, quantity) values ($1, $2, $3)",
             add
