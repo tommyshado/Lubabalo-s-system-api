543f359732af5168e05cfcb13ac99563ba8b11b0
Mthunzi Tom <107440311+tommyshado@users.noreply.github.com>
2023-10-26 09:43:09 +0200

    Removed unnecessary comments

--- services/shoesService.js
+++ services/shoesService.js
@@ -1,13 +1,9 @@
 
 
 const shoesService = database => {
-    // CREATE a function that gets all the available shoes from the database
+    
     const getShoes = async () => await database.manyOrNone("select * from stock_inventory order by shoe_name");
-
-    // CREATE a function that takes in an object of { shoeName, qty, shoePrice, shoeColor },
-        // which comes from the adding of a new shoe page req.body AND...
-        // INSERT the shoe object if it's not in the database
-
+    
     const insertShoe = async (shoe) => {
         const data = [
             shoe.shoeName,
@@ -31,7 +27,6 @@ const shoesService = database => {
 
     const insertShoeHelper = async (shoe) => await database.manyOrNone(`select * from stock_inventory where image = '${shoe.image}'`);
 
-    // CREATE a function that takes in an id as params and delete a shoe from the database using the given id FIRST...
     const updateInventory = async (shoeId) => {
         await database.none(`update stock_inventory set shoe_qty = shoe_qty - 1 where shoe_id = ${shoeId} and shoe_qty > 0`);
     };
@@ -40,20 +35,13 @@ const shoesService = database => {
         await database.none(`update stock_inventory set shoe_qty = shoe_qty + 1 where shoe_id = ${shoeId}`);
     };
 
-    // CREATE a function to DELETE a shoe from the shoes database using a given shoe_id
     const deleteShoe = async (shoeId) => await database.oneOrNone(`delete from stock_inventory where shoe_id = ${shoeId} and shoe_qty = 0 RETURNING shoe_id`);
 
-    // CREATE a function that takes in a brand name which comes from the params AND...
     const getShoeBrand = async (brandname) => await database.manyOrNone(`select * from stock_inventory where shoe_name like '%${brandname}%'`);
-        // RETURN all the shoes for a given brand
 
-    // CREATE a function that takes in a size AND...
     const getShoeBySize = async (shoeSize) => await database.manyOrNone(`select * from stock_inventory where shoe_size = '${shoeSize}'`);
-        // RETURN all the shoes for a given size
 
-    // CREATE a function that takes in a brand and size as objects AND...
     const getShoeBySizeAndBrand = async (shoe) => await database.manyOrNone(`select * from stock_inventory where shoe_size = '${shoe.shoeSize}' and shoe_name = '${shoe.shoeName}'`);
-        // RETURN all the shoes for a given brand and size
 
     const filterByColor = async (color) => await database.manyOrNone(`select * from stock_inventory where shoe_color = '${color}'`);
 
